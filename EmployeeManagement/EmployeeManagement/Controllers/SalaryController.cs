10c662eaf0c3c5a762ce97d6e7d2abd94b0abb14
Sumit Kokal <sumit.kokal@nitorinfotech.com>
2023-01-10 20:12:26 +0530

    changes

---
+++ EmployeeManagement/EmployeeManagement/Controllers/SalaryController.cs
@@ -0,0 +1,40 @@
+﻿using EmployeeManagement.Context;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using EmployeeManagement.CustomSessions;
+using System.Collections.Generic;
+
+namespace EmployeeManagement.Controllers
+{
+    public class SalaryController : Controller
+    {
+        EmployeeContext _employeeContext;
+        public SalaryController(EmployeeContext employeeContext)
+        {
+            _employeeContext = employeeContext;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var empList = await _employeeContext.employees.ToListAsync();
+            var EmpSelect = new List<SelectListItem>();
+            foreach (var item in empList)
+            {
+                EmpSelect.Add(new SelectListItem(item.FirstName + " " + item.LastName, item.EmployeeId.ToString()));
+            }
+            ViewBag.RoleList = null;
+            HttpContext.Session.SetSessionObject<List<SelectListItem>>("EmployeeList", EmpSelect);
+            ViewBag.EmployeeList = EmpSelect;
+            return View();
+        }
+    }
+}
---
+++ EmployeeManagement/EmployeeManagement/Models/SalaryModel.cs
@@ -0,0 +1,22 @@
+﻿namespace EmployeeManagement.Models
+{
+    public class SalaryModel
+    {
+        public int EmployeeId { get; set; }
+        public int BasicPay { get; set; }
+        public int HRA { get; set; }
+        public int TA { get; set; }
+        public int DA { get; set; }
+        public int OverTime { get; set; }
+        public int WeekendWorked { get; set; }
+    }
+
+    public enum Calculation
+    {
+        HRA = 500,
+        TA = 400,
+        DA = 300,
+        OverTime = 500,
+        WeekendWorked = 800
+    }
+}
